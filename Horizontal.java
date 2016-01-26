fe3b5c06702a1cd37dae845beaae436478ac96dc
samuelmesfin16 <samuelmesfin16@mittymonarch.com>
2016-01-26 10:40:52 -0800

    bump

--- Horizontal.java
+++ Horizontal.java
@@ -1,5 +1,16 @@
 
 public class Horizontal extends Target
 {
-    
+    int m = 0;
+    public Horizontal(int x, int y, int m) 
+    {
+        this.x = x;
+        this.y = y;
+        this.m = m;
+    }
+    public void move()
+    {
+       m = (double)(y-250)/(x-500);
+       
+    }
 }
